37064ae8bb4182270d12b180a8067c342585654c
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2021-12-30 10:43:47 +0700

    Up

---
+++ CosmeticsShop/CosmeticsShop/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using CosmeticsShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CosmeticsShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        ShoppingEntities db = new ShoppingEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CosmeticsShop/CosmeticsShop/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using CosmeticsShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CosmeticsShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        ShoppingEntities db = new ShoppingEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            ViewBag.ListCategory = db.Categories.ToList();
+            ViewBag.ListProduct = db.Products.ToList();
+            return View();
+        }
+        public ActionResult Details(int ID)
+        {
+            ViewBag.ListCategory = db.Categories.ToList();
+            Product product = db.Products.Single(x => x.ID == ID);
+            return View(product);
+        }
+    }
+}
